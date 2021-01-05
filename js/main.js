6d38fc3d8b2aa18ae06bf7e98fec489aa37e0874
samgarciav <samgraciav@gmail.com>
2021-01-04 18:14:23 -0600

    Created the small CSS and add some styling

--- js/main.js
+++ js/main.js
@@ -2,5 +2,22 @@ const links = [
   {
     label: "Week1 notes",
     url: "week1/index.html"
+  },
+  {
+    label: "Week 2",
+    url: "week2/index.html"
   }
-]
+];
+
+var ol = document.getElementsByTagName('ol')[0];
+
+links.forEach((el,i)=>{
+let li = document.createElement('li');
+let a = document.createElement('a');
+ol.appendChild(li);
+a.setAttribute('href', links[i].url);
+li.appendChild(a);
+a.innerHTML = links[i].label;
+});
+
+
