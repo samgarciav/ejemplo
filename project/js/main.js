c432353bc9f5b0eb1e5fc7972f12c822f81e293a
samgarciav <samgraciav@gmail.com>
2021-04-03 22:59:48 -0600

    starting the Sort future

--- project/js/main.js
+++ project/js/main.js
@@ -1,36 +1,47 @@
+/* -------------------------  Hamburger Button  ------------------------- */
 window.onload = toggleNav;
-
 function toggleNav() {
-  var hamburguer = document.getElementById("hamburger");
+  const hamburguer = document.getElementById("hamburger");
   const menuList = document.querySelector('.menu-list');
   hamburguer.addEventListener("click", () => {
     hamburguer.classList.toggle("open");
     menuList.classList.toggle("open");
+    document.body.classList.toggle("overflow");
   });
 }
 
+/* -------------------------  Single Product Details  ------------------------- */
 const thisfunction = function () {
   console.log("hello");
 }
-
-
 const singleproduct = document.querySelectorAll(".singleProduct");
 console.log(singleproduct);
 singleproduct.forEach(product => { product.addEventListener("click", thisfunction) })
 
 
-
+/* -------------------------  Toogle Views  ------------------------- */
 const changeView = document.querySelector(".changeView");
 const productsGrid = document.querySelector(".productsGrid");
 changeView.addEventListener("click", () => {
   console.log(changeView.innerHTML);
   if (changeView.innerText == "List") {
-    changeView.innerHTML=`<img src="images/mosaic.svg" alt="View Icon">Mosaic`;
+    changeView.innerHTML = `<img src="images/mosaic.svg" alt="View Icon">Mosaic`;
   }
-  else if((changeView.innerText == "Mosaic")){
-    changeView.innerHTML=`<img src="images/list.svg" alt="View Icon">List`;
+  else if ((changeView.innerText == "Mosaic")) {
+    changeView.innerHTML = `<img src="images/list.svg" alt="View Icon">List`;
   }
   productsGrid.classList.toggle('block')
+});
 
-
+/* -------------------------  Sort  ------------------------- */
+const sort = document.querySelector(".sort");
+const sortHide = document.querySelector(".sort-hide");
+const backArrow = document.querySelector(".backArrow");
+sort.addEventListener("click", () => {
+  sortHide.classList.toggle("sort-show")
+  document.body.classList.toggle("overflow");
+});
+backArrow.addEventListener("click", () => {
+  sortHide.classList.toggle("sort-show")
+  document.body.classList.toggle("overflow");
 });
